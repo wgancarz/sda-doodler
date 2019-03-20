40a966d8056a0a30d2993cc4361485fc8cf16425
Gancarz, Wojciech <wojciech.gancarz@sap.com>
2019-03-20 21:06:36 +0100

    Moved Patient comparison logic to PatientComparator.

--- hospital/src/main/java/hospital/model/Patient.java
+++ hospital/src/main/java/hospital/model/Patient.java
@@ -1,37 +1,10 @@
 package hospital.model;
 
-public class Patient implements Comparable<Patient> {
+public class Patient {
     String firstName;
     String lastName;
     Integer angerLevel;
     Disease disease;
-
-    private int getPriority() {
-        return angerLevel * disease.getContagiousness();
-    }
-
-    @Override
-    public int compareTo(Patient otherPatient) {
-        final String PRIORITY_LASTNAME = "Kowalski";
-
-        if ((this.getLastName().equalsIgnoreCase(PRIORITY_LASTNAME))
-                && !(otherPatient.getLastName().equalsIgnoreCase(PRIORITY_LASTNAME))) {
-            return -1;
-        }
-
-        if ((otherPatient.getLastName().equalsIgnoreCase(PRIORITY_LASTNAME))
-                && !(this.getLastName().equalsIgnoreCase(PRIORITY_LASTNAME))) {
-            return 1;
-        }
-
-        if (this.getPriority() > otherPatient.getPriority()) {
-            return -1;
-        } else if (this.getPriority() == otherPatient.getPriority()) {
-            return 0;
-        } else {
-            return 1;
-        }
-    }
 
     @Override
     public String toString() {
