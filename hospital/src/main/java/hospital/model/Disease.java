1396156df451acc9fecb0c34122516baed80be14
Gancarz, Wojciech <wojciech.gancarz@sap.com>
2019-03-20 20:19:00 +0100

    Sorted diseases alphabetically to show independence of values() from getContagiousness()

--- hospital/src/main/java/hospital/model/Disease.java
+++ hospital/src/main/java/hospital/model/Disease.java
@@ -1,9 +1,9 @@
 package hospital.model;
 
 public enum Disease {
-    INFLUENZA(1),
     COLD(2),
     DIARRHEA(3),
+    INFLUENZA(1),
     SOMETHING_SERIOUS(4);
 
     private Integer contagiousness;
