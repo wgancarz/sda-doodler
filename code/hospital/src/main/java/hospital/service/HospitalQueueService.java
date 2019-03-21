ed9fab17bfb882519db4ebea7132ed871b7b7c57
Gancarz, Wojciech <wojciech.gancarz@sap.com>
2019-03-21 17:11:38 +0100

    Using PatientComparator in queue explicitly.

--- code/hospital/src/main/java/hospital/service/HospitalQueueService.java
+++ code/hospital/src/main/java/hospital/service/HospitalQueueService.java
@@ -1,11 +1,12 @@
 package hospital.service;
 
 import hospital.model.Patient;
+import hospital.model.comparator.PatientComparator;
 
 import java.util.PriorityQueue;
 
 public class HospitalQueueService {
-    PriorityQueue<Patient> queue = new PriorityQueue<>();
+    PriorityQueue<Patient> queue = new PriorityQueue<>(new PatientComparator());
 
     public void add(Patient patient) {
         queue.add(patient);
